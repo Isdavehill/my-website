38e147ac92958c703d019e326090c37ed9858452
Dave Hill <99537934+Isdavehill@users.noreply.github.com>
2023-05-02 13:03:43 +0100

    Initial commit

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lsng="en">
+            <Head>
+                
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
